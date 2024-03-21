da5c964f35306894ef2dec6981effb4b22755607
Pavlo Kyrychenko <81436169+oldstuden@users.noreply.github.com>
2024-03-21 16:28:56 +0100

    created module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    AuthModule,
+    MongooseModule.forRoot(`mongodb+srv://Pawel:jG5fYL5oPAXAOM04@cluster0.yhypksh.mongodb.net/my-contacts?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
