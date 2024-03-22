43d68484744fcdc6b0a5afc417ea3991064d10ea
NataDent <cherkashyna.n@gmail.com>
2024-03-22 14:25:42 +0200

    fixed app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module( {
   imports: [
-    MongooseModule.forRoot( 'mongodb+srv://Nataly:mFid220821@cluster0.bjaedo8.mongodb.net/db-test-shop?retryWrites=true&w=majority' ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot( process.env.MONGODB_URI ),
     ProductsModule,
   ],
 } )
